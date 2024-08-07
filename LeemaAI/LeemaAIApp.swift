4711da82ee54068040d7eb142e79977c7965e39f
zikrya <77081809+zikrya@users.noreply.github.com>
2024-08-06 23:57:42 -0400

    Initial Commit

---
+++ LeemaAI/LeemaAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeemaAIApp.swift
+//  LeemaAI
+//
+//  Created by Zikrya Javed on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LeemaAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
